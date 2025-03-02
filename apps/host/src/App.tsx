8bf824578a9738a87bac46b1f368cd255e58e10e
imevanc <charal.vag@gmail.com>
2025-03-02 16:40:20 +0000

    feat: update docs

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="content">
       <h1 className="text-sm text-purple-400">Host</h1>{" "}
-      {/* Added text-purple-400 which is the random red*/}
+      {/* Added text-purple-400 which is the random red */}{" "}
       <Button appName="react-host">Click me</Button>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
