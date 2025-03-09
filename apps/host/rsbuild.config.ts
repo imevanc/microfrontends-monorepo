f271e16b218f143d117a735949ea3ff9c55afdeb
imevanc <charal.vag@gmail.com>
2025-03-09 20:41:03 +0000

    feat: link remote with turborepo pkgs

--- apps/host/rsbuild.config.ts
+++ apps/host/rsbuild.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+
+const getRemoteEntryUrl = () =>
+  process.env.VERCEL_ENV === "production"
+    ? "https://microfrontends-monorepo.vercel.app/mf-manifest.json"
+    : "http://localhost:3001/mf-manifest.json";
+
+console.log("getRemoteEntryUrl", getRemoteEntryUrl());
 
 export default defineConfig({
   plugins: [
@@ -8,12 +19,17 @@ export default defineConfig({
     pluginModuleFederation({
       name: "host",
       remotes: {
-        journeya: "journeya@http://localhost:3001/mf-manifest.json",
+        journeya: `journeya@${getRemoteEntryUrl()}`,
       },
       shared: ["react", "react-dom"],
     }),
   ],
   server: {
     port: 3000,
+  },
+  source: {
+    define: {
+      "process.env.VERCEL_ENV": JSON.stringify(process.env.VERCEL_ENV || ""),
+    },
   },
 });
